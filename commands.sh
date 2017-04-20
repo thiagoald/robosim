a4f7e6058e4a183e9916dbe12d72137f755464b9
osboxes.org <osboxes@localhost.localdomain>
2017-04-20 16:55:38 -0300

    first commit

---
+++ commands.sh
@@ -0,0 +1,3 @@
+# MobileSim -m /usr/local/MobileSim/columbia.map &
+# g++ actionExample.cpp -o example1 -I/usr/local/Aria/include/ -Wl,-rpath,/usr/local/Aria/lib -L /usr/local/Aria/lib/ -lAria
+# ./example1
